657d9a76072d17d03691dbec0e9a84ec61851420
Andrew <andrew.abi.mansour@merck.com>
2016-06-24 16:18:59 -0400

    updated running script

--- run.py
+++ run.py
@@ -16,32 +16,33 @@ if __name__ == '__main__':
 			  'dim': 3,
 			  'style': 'granular', # spherical deformable particles
 			  'boundary': ('f','f','f'), # fixed BCs
-			  'model': ('gran', 'model', 'hooke', 'tangential', 'history', 'tangential_damping', 'on',),
-			  'box':  (-0.032, 0.032, -0.032, 0.032, -0.01, 0.122), # simulation box size
+			  'model': ('gran', 'model', 'hooke', 'tangential', 'history', 'rolling_friction', 'cdt', 'tangential_damping', 'on', 'limitForce', 'on'), # the order matters here
+			  'box':  (-0.016, 0.016, -0.016, 0.016, -0.01, 0.061), # simulation box size
 
 			  # Define component(s)
-			  'SS': ({'id':1, 'natoms': 5e3, 'density': 2500, 'insert': True, 'rate':10**6, 'freq': 10**3}, ), # rate of particles inserted = rate x dt x freq
+			  'SS': ({'id':1, 'natoms': 2.5e5, 'density': 2500, 'insert': True, 'rate':10**6, 'freq': 10**3}, ), # rate of particles inserted = rate x dt x freq
 			  'nSS': 2, 
-			  'vel': ((0,0.0,0), ),
-			  'radius': (('constant', 4.48e-4),),
+			  'vel': ((0,0,0), ),
+			  'radius': (('gaussian number', 4.0e-4, 4.0e-5),),
 
 			  # Apply gravitional force in the negative direction along the z-axis
 			  'gravity': (9.81, 0, 0, -1), 
 
 			  # I/O parameters
-			  'restart': (10**4, 'restart', 'restart.*', True),
-			  'traj': {'sel':'all', 'freq':10000, 'dir':'traj', 'file':'traj.custom', 'args': ('x', 'y', 'z', 'radius', 'omegax', 'omegay', 'omegaz', 'fx', 'fy', 'fz')},
+			  'restart': (5000, 'restart', 'restart.*', False),
+			  'traj': {'sel': 'all', 'freq': 1000, 'dir': 'traj', 'file': 'traj.custom', 'args': ('id', 'x', 'y', 'z', 'radius', 'omegax', 'omegay', 'omegaz', 'vx', 'vy', 'vz', 'fx', 'fy', 'fz')},
+			  'dump_modify': ('append', 'yes'),
 			  'nSim': 1,
-			  'output': 'out-5e5-1cores',
-			  'print': (10**4, 'time', 'atoms', 'fmax', 'ke', 'cpu', 'cu'), # print the time, atom number, avg. kinetic energy, and max force
+			  'output': 'out-radius-400-gaussian-roll-0.9-0.9',
+			  'print': (10**4, 'time', 'atoms', 'fmax', 'ke', 'cpu', 'cu', 'density'), # print the time, atom number, avg. kinetic energy, and max force
 
 			  # Stage runs
-			  'insertion':  {'steps':  2 * 10**4, 'dt': 10**-5},
-			  'flow': {'steps': 10**4, 'dt': 10**-5},
+			  'insertion':  {'steps':  5e4, 'dt': 1e-5},
+			  'flow': {'steps': 3e5, 'dt': 2e-5},
 
 			  # Meshes
 			  'surfMesh': {
-					'hopper': {'file': 'hopper.stl', 'scale':0.001},
+					'hopper': {'file': 'hopper-2cm-6cm.stl', 'scale':5e-4},
 				      },
 
 			  # Nearest neighbor searching paramers
@@ -61,7 +62,8 @@ if __name__ == '__main__':
 	# Setup material properties
 	sim.createProperty('mYmod', *('youngsModulus', 'peratomtype', '7.1e7', '7.1e7'))
 	sim.createProperty('mPratio', *('poissonsRatio', 'peratomtype', '0.22', '0.22'))
-	sim.createProperty('mCfric', *('coefficientFriction', 'peratomtypepair', '2', '0.1', '0.1', '0.1', '0.1'))
+	sim.createProperty('mCfric', *('coefficientFriction', 'peratomtypepair', '2', '0.1', '0.9', '0.9', '0.9'))
+	sim.createProperty('mRfric', *('coefficientRollingFriction', 'peratomtypepair', '2', '0.9', '0.9', '0.9', '0.9'))
 	sim.createProperty('mCvel', *('characteristicVelocity', 'scalar', '0.01', '0.01'))
 
 	# For 'm' simulations, we need 'm' tuples for the coefficient of restitution
@@ -70,12 +72,10 @@ if __name__ == '__main__':
 	# Overloaded function 'createProperty' will partition coeffRest based on MPI's coloring split scheme
 	sim.createProperty('mCrest', *coeffRest)
 
-	# Import all meshes
+	# Import and setup all meshes as rigid wall
 	for mesh in params['surfMesh'].keys():
 		sim.importMesh(name=mesh, **params['surfMesh'][mesh])
-
-	# Setup hopper mesh as a wall
-	sim.setupWall(name='hopper', wtype='mesh')
+		sim.setupWall(name=mesh + 'Wall', wtype='mesh', meshName=mesh)
 
 	# Setup a stopper wall along the xoz plane (y = 0.0)
 	sim.setupWall(name='stopper', wtype='primitive', plane = 'zplane', peq = 0.0)
@@ -89,25 +89,13 @@ if __name__ == '__main__':
 	# Write 'all' coordinates to 'traj.xyz' file every 'freq' steps
 	sim.dumpSetup()
 
-	# Insert particles for stage 1
-	# lowerSphere = sim.insertParticles('lowerSphere', *('sphere', 0, 0, 0.03, 0.03))
-	# sim.integrate(**params['insertion'])
-	# sim.remove(lowerSphere)
-
-	# Insert particles for stage 2
-	# midSphere = sim.insertParticles('midSphere', *('sphere', 0, 0, 0.08, 0.03))
-	# sim.integrate(**params['insertion'])
-	# sim.remove(midSphere)
+	if not params['restart'][3]:
+		# Insert particles for stage 1
+		cylinder = sim.insertParticles('void', *('cylinder', 'z', 0, 0, 0.014, 0.005, 0.055))
+		sim.integrate(**params['insertion'])
+		sim.remove(cylinder)
 
 	# Remove stopper
 	sim.remove(name='stopper')
 
 	sim.integrate(**params['flow'])
-
-	# Monitor translational and rotational <KE>
-   	sim.monitor(name='ke', group='all', var='globKE', file='ke.dat')
-	sim.monitor(name='erotate/sphere', group='all', var='globRE', file='re.dat')
-
-	# Save rotational KE to dat file
-	sim.plot(fname='ke.dat', xlabel='Time (s)', ylabel='KE (J/atom)', output='ke.pdf', xscale = params['flow']['dt'])
-	sim.plot(fname='re.dat', xlabel='Time (s)', ylabel='RE (J/atom)', output='re.pdf', xscale = params['flow']['dt'])
