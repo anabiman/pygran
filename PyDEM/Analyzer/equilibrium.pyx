9c86c6302082434ecfe22c65df54478caec0a6d9
Andrew-AbiMansour <andrew.abi.mansour@merck.com>
2016-09-08 15:56:47 -0400

    cythonized equilibrium module

--- PyDEM/Analyzer/equilibrium.pyx
+++ PyDEM/Analyzer/equilibrium.pyx
@@ -26,14 +26,8 @@ Created on July 10, 2016
 
 # -------------------------------------------------------------------------
 
-from numpy import zeros, sqrt, where, pi, mean, arange, histogram, array, dot, sqrt
-
-try:
-	from xlrd import open_workbook
-except:
-	pass
-
-from numbers import Number
+import numpy as np
+cimport numpy as np
 
 def computeROG(x, y, z):
 	""" Computes the radius of gyration (ROG) for an N-particle system:
@@ -41,25 +35,25 @@ def computeROG(x, y, z):
 	particles, and <...> is the ensemble average. Alternatively, one can
 	compute ROG as \sum_i <r_i^T r_i> - rm ^T rm
 	"""
-	rm = array([x.mean(), y.mean(), z.mean()])
+	rm = np.array([x.mean(), y.mean(), z.mean()])
 	N = len(rm)
 
-	return sqrt((dot(x,x) + dot(y,y) + dot(z,z)) / N - dot(rm, rm))
+	return np.sqrt((np.dot(x,x) + np.dot(y,y) + np.dot(z,z)) / N - np.dot(rm, rm))
 
 def computeRadius(x, y, z, N = 100):
 	""" Computes the maximum radius of an N-particle (spherical) system
 	by sorting the radial components and returning the average of the sqrt
 	of the radius of the first N max data points. 
 	"""
-	rm = array([x.mean(), y.mean(), z.mean()])
+	rm = np.array([x.mean(), y.mean(), z.mean()])
 
 	r = ((x - rm[0])**2.0 + (y - rm[1])**2.0 + (z - rm[2])**2.0)
 	r.sort()
 
-	return sqrt(r[-N:]).mean()
+	return np.sqrt(r[-N:]).mean()
 
 def computeRDF(x, y, z, dr = None, center = True, rMax=None):
-    """ Computes the three-dimensional radial distribution function for a set of
+	""" Computes the three-dimensional radial distribution function for a set of
     spherical particles contained in a cube with side length S.  This simple
     function finds reference particles such that a sphere of radius rMax drawn
     around the particle will fit entirely within the cube, eliminating the need
@@ -81,22 +75,22 @@ def computeRDF(x, y, z, dr = None, center = True, rMax=None):
         reference_indices   indices of reference particles
     """
     
-    # center positions around 0 \\uswsps0002\USWPPR2041
-    if center:
-    	x -= x.mean()
-    	y -= y.mean()
-    	z -= z.mean()
+	# center positions around 0 \\uswsps0002\USWPPR2041
+	if center:
+		x -= x.mean()
+		y -= y.mean()
+		z -= z.mean()
 
-    S = min(x.max(), y.max(), z.max())
+	S = min(x.max(), y.max(), z.max())
 
-    if rMax is None:
-    	rMax = S / 2.0
+	if rMax is None:
+		rMax = S / 2.0
 
-    if dr is None:
-    	dr = rMax / 100
+	if dr is None:
+		dr = rMax / 100
 
-    # Find particles which are close enough to the cube center that a sphere of radius
-    # rMax will not cross any face of the cube
+	# Find particles which are close enough to the cube center that a sphere of radius
+	# rMax will not cross any face of the cube
 
 	print 'Constructing a cube of length {} and a circumscribed sphere of radius {}'.format(S * 2.0, rMax)
 	print 'Resolution chosen is {}'.format(dr)
@@ -108,66 +102,38 @@ def computeRDF(x, y, z, dr = None, center = True, rMax=None):
 	bools5 = z > rMax - S
 	bools6 = z < (S - rMax)
 
-	interior_indices, = where(bools1 * bools2 * bools3 * bools4 * bools5 * bools6)    
-    num_interior_particles = len(interior_indices)
+	interior_indices, = np.where(bools1 * bools2 * bools3 * bools4 * bools5 * bools6)    
+	num_interior_particles = len(interior_indices)
 
-    if num_interior_particles < 1:
-        raise  RuntimeError ("No particles found for which a sphere of radius rMax will lie entirely within a cube of side length S.  Decrease rMax or increase the size of the cube.")
+	if num_interior_particles < 1:
+		raise  RuntimeError ("No particles found for which a sphere of radius rMax will lie entirely within a cube of side length S.  Decrease rMax or increase the size of the cube.")
 
-    edges = arange(0., rMax + 1.1 * dr, dr)
-    num_increments = len(edges) - 1
-    g = zeros([num_interior_particles, num_increments])
-    radii = zeros(num_increments)
-    numberDensity = len(x) / S**3
+	edges = np.arange(0., rMax + 1.1 * dr, dr)
+	num_increments = len(edges) - 1
+	g = np.zeros([num_interior_particles, num_increments])
+	radii = np.zeros(num_increments)
+	numberDensity = len(x) / S**3
 
-    # Compute pairwise correlation for each interior particle
-    for p in range(num_interior_particles):
-        index = interior_indices[p]
-        d = sqrt((x[index] - x)**2 + (y[index] - y)**2 + (z[index] - z)**2)
-        d[index] = 2 * rMax
+	# Compute pairwise correlation for each interior particle
+	for p in range(num_interior_particles):
+		index = interior_indices[p]
+		d = np.sqrt((x[index] - x)**2 + (y[index] - y)**2 + (z[index] - z)**2)
+		d[index] = 2 * rMax
 
-        (result, bins) = histogram(d, bins=edges, normed=False)
-        g[p,:] = result / numberDensity
+		(result, bins) = np.histogram(d, bins=edges, normed=False)
+		g[p,:] = result / numberDensity
 
-    # Average g(r) for all interior particles and compute radii
-    g_average = zeros(num_increments)
-    for i in range(num_increments):
-        radii[i] = (edges[i] + edges[i+1]) / 2.
-        rOuter = edges[i + 1]
-        rInner = edges[i]
-        g_average[i] = mean(g[:, i]) / (4.0 / 3.0 * pi * (rOuter**3 - rInner**3))
+	# Average g(r) for all interior particles and compute radii
+	g_average = np.zeros(num_increments)
+	for i in range(num_increments):
+		radii[i] = (edges[i] + edges[i+1]) / 2.
+		rOuter = edges[i + 1]
+		rInner = edges[i]
+		g_average[i] = np.mean(g[:, i]) / (4.0 / 3.0 * np.pi * (rOuter**3 - rInner**3))
 
-    return (g_average, radii, interior_indices)
-    # Number of particles in shell/total number of particles/volume of shell/number density
-    # shell volume = 4/3*pi(r_outer**3-r_inner**3)
-
-def readExcel(fname):
-
-	"""
-	reads an excel sheet(s) and appends each data column to 
-	a dictionary
-	"""
-	book = open_workbook(fname, on_demand=True)
-	data = {}
-
-	for name in book.sheet_names():
-		sheet = book.sheet_by_name(name)
-
-		for coln in range(sheet.ncols):
-			for celln, cell in enumerate(sheet.col(coln)):
-				if celln == 0:
-					dname = cell.value
-					data[dname] = []
-				else:
-					if isinstance(cell.value, Number):
-						data[dname].append(cell.value)
-					else:
-						print cell.value, ' ignored'
-
-	for key in data.keys():
-		data[key] = array(data[key])
-
-	return data
+	return (g_average, radii, interior_indices)
+	# Number of particles in shell/total number of particles/volume of shell/number density
+	# shell volume = 4/3*pi(r_outer**3-r_inner**3)
 
 def computeAngleRepose(data, *args):
 	"""
@@ -231,23 +197,3 @@ def computeDensity(data, density, shape = 'box', sel = None):
 	mass = np.sum(density * 4.0 / 3.0 * np.pi * (data['radius'][sel])**3.0)
 
 	return mass / volume
-
-def computeHeight(data, axis):
-	"""
-	Computes the mean max height of an N-particle system along the x, y, or z axis.
-	"""
-	height = data[axis].max()
-	hmin = height * 0.99
-	hmax = height * 1.01
-
-	if axis == 'x':
-		region = (hmin, hmax, -np.inf, np.inf, -np.inf, np.inf)
-	elif axis == 'y':
-		region = (-np.inf, np.inf, hmin, hmax, -np.inf, np.inf)
-	elif axis == 'z':
-		region = (-np.inf, np.inf, -np.inf, np.inf, hmin, hmax)
-	else:
-		print "axis must be x, y, or z"
-		raise
-
-	return data[axis][select(data, *region)].mean()
---
+++ PyDEM/Analyzer/tools.py
@@ -0,0 +1,33 @@
+try:
+	from xlrd import open_workbook
+except:
+	pass
+
+from numbers import Number
+
+def readExcel(fname):
+	"""
+	reads an excel sheet(s) and appends each data column to 
+	a dictionary
+	"""
+	book = open_workbook(fname, on_demand=True)
+	data = {}
+
+	for name in book.sheet_names():
+		sheet = book.sheet_by_name(name)
+
+		for coln in range(sheet.ncols):
+			for celln, cell in enumerate(sheet.col(coln)):
+				if celln == 0:
+					dname = cell.value
+					data[dname] = []
+				else:
+					if isinstance(cell.value, Number):
+						data[dname].append(cell.value)
+					else:
+						print cell.value, ' ignored'
+
+	for key in data.keys():
+		data[key] = array(data[key])
+
+	return data
