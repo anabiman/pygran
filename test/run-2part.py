6255760c3d6770d44eb7cd37e42573506484e9af
Andrew-AbiMansour <andrew.gaam@gmail.com>
2016-09-28 17:33:24 -0400

    tested two-component systems

--- test/run-2part.py
+++ test/run-2part.py
@@ -21,28 +21,28 @@ if __name__ == '__main__':
 			'boundary': ('f','f','f'), # fixed BCs
 			'box':  (-0.016, 0.016, -0.016, 0.016, -0.01, 0.061), # simulation box size
 
-			# 'model-args': ('gran', 'model', 'hooke', 'limitForce', 'on', 'ktToKnUser', 'on'),
-
 			# Define component(s)
-			'SS': ({'id':1, 'natoms': 10000, 'density': 2500.0, 'insert': True, 'rate': 1e6, \
-				'freq': 100, 'radius': ('gaussian number', 4e-4, 4e-5)}, 
+			'SS': ({'id':1, 'natoms': 1, 'density': 2500.0, 'insert': True, 'rate': 1e6, \
+				'freq': 100, 'radius': ('gaussian number', 20e-4, 4e-5)}, 
+				{'id':2, 'natoms': 1, 'density': 2500.0, 'insert': True, 'rate': 1e6, \
+				'freq': 100, 'radius': ('gaussian number', 20e-4, 4e-5)}
 			      ), 
 				# number of particles inserted = rate * dt * freq every freq steps
 
-			'traj': {'sel': 'all', 'freq': 1000, 'dir': 'traj', 'style': 'custom', 'file': 'traj.dump', \
+			'traj': {'sel': 'all', 'freq': 1, 'dir': 'traj', 'style': 'custom', 'file': 'traj.dump', \
                        'args': ('id', 'x', 'y', 'z', 'radius', 'omegax', 'omegay', 'omegaz', \
                        'vx', 'vy', 'vz', 'fx', 'fy', 'fz')},
 
-			'dt': 1e-05,
+			'dt': 3e-05,
 
 			# Material properties
 			'materials': glass,
 
 			# Apply gravitional force in the negative direction along the z-axis
-			'gravity': (9.81, 0, 0, -1),
+			'gravity': (9.81, 0, 0, 1),
 
 			# Stage runs
-			'stages': {'insertion': 2e4, 'run': 0e6},
+			'stages': {'insertion': 50, 'run': 0e6},
 
 			# Meshes
 			'mesh': {
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 			      },
 		  }
 
-	# Instantiate a class based on the selected model
+	# Instantiate a class based on a user-defined model
 	pDict['model'] = pDict['model'](**pDict)
 
 	# Create an instance of the DEM class
@@ -60,5 +60,6 @@ if __name__ == '__main__':
 	sim.setupWall(name='stopper', wtype='primitive', plane = 'zplane', peq = 0.0)
 
 	sim.insert('void', 1, *('cylinder', 'z', 0, 0, 0.01, 0.01, 0.04001))
+	sim.insert('void2', 2, *('cylinder', 'z', 0, 0, 0.01, 0.0, 0.00001))
 
 	sim.run(pDict['stages']['insertion'], pDict['dt'])
