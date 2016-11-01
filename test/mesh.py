15e9968cabdea40c86431c2fe4a50d93e359e62f
Andrew-AbiMansour <andrew.abi.mansour@merck.com>
2016-10-31 23:26:27 -0400

    added surface mesh

--- test/mesh.py
+++ test/mesh.py
@@ -2,17 +2,44 @@ import numpy
 from stl import mesh
 
 # Create 3 faces of a cube
-data = numpy.zeros(6, dtype=mesh.Mesh.dtype)
-scale = 20.0
-z = 30.0
+scale = 30.0
+z = 60.0
 
+tris2D1 = numpy.loadtxt('postris1.dat', delimiter=',') * scale
+tris2D2 = numpy.loadtxt('postris2.dat', delimiter=',') * scale
+tris2D3 = numpy.loadtxt('postris3.dat', delimiter=',') * scale
+
+tris2D1[:,1] *= 2.0
+tris2D2[:,1] *= 2.0
+tris2D3[:,1] *= 2.0
+
+cog = (tris2D1.sum(axis=0) + tris2D2.sum(axis=0) + tris2D3.sum(axis=0)) / len(tris2D1)
+print cog
+
+tris2D1 -= cog
+tris2D2 -= cog
+tris2D3 -= cog
+
+
+data = numpy.zeros(6 * len(tris2D1), dtype=mesh.Mesh.dtype)
+
+for i in range(len(tris2D1)):
+
+	x1, y1 = tris2D1[i]
+	x2, y2 = tris2D2[i]
+	x3, y3 = tris2D3[i]
+
+	data['vectors'][i] = numpy.array([[x1, y1, z],
+					 [x2, y2, z],
+					 [x3, y3, z]])
 # Top of the cube
-data['vectors'][0] = numpy.array([[-0.5, 0.5, z/scale],
-                                  [0.5, -0.5, z/scale],
-                                  [-0.5, -0.5, z/scale]]) * scale
-data['vectors'][1] = numpy.array([[0.5, -0.5, z/scale],
-                                  [-0.5, 0.5, z/scale],
-                                  [0.5, 0.5, z/scale]]) * scale
+#data['vectors'][0] = numpy.array([[-0.5, 0.5, z/scale],
+#                                  [0.5, -0.5, z/scale],
+#                                  [-0.5, -0.5, z/scale]]) * scale
+
+#data['vectors'][1] = numpy.array([[0.5, -0.5, z/scale],
+#                                  [-0.5, 0.5, z/scale],
+#                                  [0.5, 0.5, z/scale]]) * scale
 
 meshes = mesh.Mesh(data)
 
