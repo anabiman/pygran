cfc677287f2fa9b6413bb6efdfa4b29fc93ae3a2
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-01-18 16:56:19 -0500

    movie maker works for multi-frames

--- test/make_move.py
+++ test/make_move.py
@@ -1,30 +1,51 @@
-import os, glob
+import os, glob, sys
 
-def make_movie(dir, img, output='out.mp4', frameRate=10):
+def make_movie(dir, imgs, nFrames = None, frameRate=10, output = 'out.mp4'):
 
-	os.system('mkdir tmp')
-	os.system('cp {}/*.png tmp/'.format(dir))
+	if not os.path.exists('tmp'):
+		os.system('mkdir tmp')
+	else:
+		print 'path tmp already exists'
+		sys.exit()
 
-	files = glob.glob("{}/*.png".format(dir))
-	files = sorted(files, key=lambda item: int( (item.split(dir+'/'+img)[1]).split('.png')[0] )) 
+	for img in imgs:
+		os.system('cp {}/{}*.png tmp/'.format(dir, img))
+
+	imgFiles = [glob.glob("{}/{}*.png".format(dir, img)) for img in imgs]
+
+	for i in range(len(imgs)):
+		imgFiles[i] = imgFiles[i][:nFrames]
+
+	files = []
+	count = 0
+
+	for file in imgFiles:
+		files.append(sorted(file, key=lambda item: int( (item.split(dir + '/' + imgs[count])[1]).split('.png')[0] )))
+		count += 1
 
 	numbers = []
-	digits = str(len(files))
+	nFiles = min([len(file) for file in files])
+	digits = str(nFiles)
 
-	for i in range(len(files)):
+	for i in range(nFiles):
 		num = str(i)
 		numbers.append('0' * ( len(digits) - len(num) ) + num)
 
-	count = 0
-	for file in files:
+	for i in range(nFiles):
+		imgf = []
 
-		imgf = 'img' + numbers[count] + '.png'
-		print file
-		os.system(""" convert -pointsize 40 -fill white -draw 'text 5,100 "Frame: {}" ' {} tmp/{} """.format(count, file, imgf))
-		count += 1
+		for img in imgs:
+			imgf.append('tmp/' + img + numbers[i] + '.png')
 
-	os.system('ffmpeg -framerate {} -i tmp/img%0{}d.png -c:v libx264 -pix_fmt yuv420p {}'.format(frameRate, len(digits), output))
+		print 'Processing image ' + numbers[i]
+		
+		imgout = 'tmp/imgout{}.png'.format(numbers[i])
+		os.system(""" convert -pointsize 40 -fill white -draw 'text 5,100 "Frame: {}" text 5, 10 " No friction" ' {} {} """.format(i, imgf[0], imgout))
+
+		os.system(""" convert -pointsize 40 -fill white +append -draw 'text 5,100 "Frame: {}" ' {} {} {} """.format(i, ' '.join(imgf[1:]), imgout, imgout))
+		
+	os.system('ffmpeg -framerate {} -i tmp/imgout%0{}d.png {}'.format(frameRate, len(digits), output))
+
 	os.system('rm -r tmp')
 
-
-make_movie(dir='Cohesion-movie', img='cohes')
+make_movie(dir='movie2', imgs = ['friction', 'nofriction', 'stages'], nFrames = 5)
