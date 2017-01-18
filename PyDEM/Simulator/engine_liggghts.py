6162745f0561d9267175caf92bfaac22db2a6fb8
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-01-18 11:21:40 -0500

    added default vol_lim

--- PyDEM/Simulator/engine_liggghts.py
+++ PyDEM/Simulator/engine_liggghts.py
@@ -361,6 +361,9 @@ class DEMPy:
         randName = np.random.randint(0,10**6)
         pddName = 'pdd' + '{}'.format(np.random.randint(0,10**6))
 
+        if 'vol_lim' not in ss:
+          ss['vol_lim'] = 1e-12
+
         self.lmp.command('group group{id} type {id}'.format(**ss))
         self.lmp.command('fix {} '.format(randName) + 'group{id} particletemplate/sphere 1 volume_limit {vol_lim} atom_type {id} density constant {density} radius'.format(**ss) + (' {}' * len(radius)).format(*radius))
         self.lmp.command('fix {} '.format(pddName) + 'group{id} particledistribution/discrete 63243 1'.format(**ss) + ' {} 1.0'.format(randName))
