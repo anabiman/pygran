74dbb897c12c91e5e8c6e31dab3f4a9de7505076
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-01-19 11:21:29 -0500

    added support for text insertion

--- test/make_move.py
+++ test/make_move.py
@@ -1,6 +1,6 @@
 import os, glob, sys
 
-def make_movie(dir, imgs, nFrames = None, frameRate=10, output = 'out.mp4'):
+def make_movie(dir, imgs, imgtxtloc = None, imgtxt = None, nFrames = None, frameRate=10, output = 'out.mp4'):
 
 	if not os.path.exists('tmp'):
 		os.system('mkdir tmp')
@@ -34,18 +34,19 @@ def make_movie(dir, imgs, nFrames = None, frameRate=10, output = 'out.mp4'):
 	for i in range(nFiles):
 		imgf = []
 
-		for img in imgs:
+		for c,img in enumerate(imgs):
 			imgf.append('tmp/' + img + numbers[i] + '.png')
+			os.system(""" convert -pointsize 40 -fill white -draw 'text {} "{}" ' {} {} """.format(imgtxtloc[c], imgtxt[c], imgf[-1], imgf[-1]))
 
 		print 'Processing image ' + numbers[i]
 		
 		imgout = 'tmp/imgout{}.png'.format(numbers[i])
-		os.system(""" convert -pointsize 40 -fill white -draw 'text 5,100 "Frame: {}" text 5, 10 " No friction" ' {} {} """.format(i, imgf[0], imgout))
-
-		os.system(""" convert -pointsize 40 -fill white +append -draw 'text 5,100 "Frame: {}" ' {} {} {} """.format(i, ' '.join(imgf[1:]), imgout, imgout))
+		os.system(""" convert -pointsize 40 -fill white +append -draw 'text 10,80 "Frame: {}" ' {} {} """.format(i, ' '.join(imgf), imgout))
 		
 	os.system('ffmpeg -framerate {} -i tmp/imgout%0{}d.png {}'.format(frameRate, len(digits), output))
 
 	os.system('rm -r tmp')
 
-make_movie(dir='movie2', imgs = ['friction', 'nofriction', 'stages'], nFrames = 5)
+# mixed up friction vs nofriction images with ovito ~ oops!
+make_movie(dir='movie', imgs = ['friction', 'nofriction', 'stages'], \
+	imgtxt = ['no friction', 'friction', 'friction (3 stages)'], imgtxtloc = ['150,990', '150, 990', '150, 990'])
