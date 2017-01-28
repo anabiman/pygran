b7a6cab974738dbe766442850c7c2095eb81d8ff
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-01-27 20:45:26 -0500

    parallelized movie processing

--- test/make_move.py
+++ test/make_move.py
@@ -1,6 +1,25 @@
-import os, glob, sys
+import os, glob, sys, multiprocessing
+from functools import partial
 
-def make_movie(dir, imgs, imgtxtloc = None, imgtxt = None, nFrames = None, frameRate=10, output = 'out.mp4', skip = None, startfrom = 0):
+def processImgs(imgs, numbers, imgFiles, imgtxt, imgtxtloc, i):
+
+	imgf = []
+
+	for c,img in enumerate(imgs):
+		imgf.append('tmp/' + img + numbers[i] + '.png')
+
+		if imgtxt:
+			os.system(""" convert -pointsize 80 -fill black -draw 'text {} "{}" ' {} {} """.format(imgtxtloc[c], imgtxt[c], imgFiles[c][i], imgf[-1]))
+		else:
+			os.system(""" cp {} {} """.format(imgFiles[c][i], imgf[-1]))
+
+	print 'Processing image ' + numbers[i]
+	
+	imgout = 'tmp/imgout{}.png'.format(numbers[i])
+
+	os.system(""" convert -pointsize 80 -fill black +append {} {} """.format(' '.join(imgf), imgout))
+
+def make_movie(dir, imgs, imgtxtloc = None, imgtxt = None, nFrames = None, frameRate=10, output = 'out.mp4', skip = None, startfrom = 0, parallel = 0):
 
 	if not os.path.exists('tmp'):
 		os.system('mkdir tmp')
@@ -20,40 +39,35 @@ def make_movie(dir, imgs, imgtxtloc = None, imgtxt = None, nFrames = None, frame
 		for i in range(len(imgs)):
 			imgFiles[i] = imgFiles[i][skip]
 
-	files = []
 	count = 0
 
-	for file in imgFiles:
-		files.append(sorted(file, key=lambda item: int( (item.split(dir + '/' + imgs[count])[1]).split('.png')[0] )))
-		count += 1
+	for i in range(len(imgFiles)):
+		imgFiles[i].sort(key=os.path.getmtime)
 
 	numbers = []
-	nFiles = min([len(file) for file in files])
+	nFiles = min([len(file) for file in imgFiles])
 	digits = str(nFiles)
 
 	for i in range(startfrom, nFiles + startfrom):
 		num = str(i)
 		numbers.append('0' * ( len(digits) - len(num) ) + num)
 
-	for i in range(nFiles):
-		imgf = []
+	if parallel:
+		pool = multiprocessing.Pool(processes=parallel)
+		func = partial(processImgs, imgs, numbers, imgFiles, imgtxt, imgtxtloc)
+		pool.map(func, range(nFiles))
+		pool.close()
+		pool.join()
+	else:
+		for i in range(nFiles):
+			processImgs(i, imgs, numbers, imgFiles, imgtxt, imgtxtloc)
 
-		for c,img in enumerate(imgs):
-			imgf.append('tmp/' + img + numbers[i] + '.png')
-			os.system(""" convert -pointsize 80 -fill white -draw 'text {} "{}" ' {} {} """.format(imgtxtloc[c], imgtxt[c], imgf[-1], imgf[-1]))
-
-		print 'Processing image ' + numbers[i]
-		
-		imgout = 'tmp/imgout{}.png'.format(numbers[i])
-		os.system(""" convert -pointsize 80 -fill white +append -draw 'text 50,80 "Frame: {}" ' {} {} """.format(i, ' '.join(imgf), imgout))
-		
-	os.system('ffmpeg -threads 2 -start_number {} -framerate {} -i tmp/imgout%0{}d.png {}'.format(startfrom, frameRate, len(digits), output))
+	os.system('ffmpeg -start_number {} -framerate {} -i tmp/imgout%0{}d.png -q:v 4 {}'.format(startfrom, frameRate, len(digits), output))
 	os.system('rm -r tmp')
 
-make_movie(dir='movie2', imgs = ['compress'])
+#make_movie(dir='movie2', imgs = ['compress'])
 
-#make_movie(dir='movie2', imgs = ['nofriction', 'friction', 'stages'], \
-#	imgtxt = ['no friction', 'friction', 'friction (3 stages)'], imgtxtloc = ['50,1970', '50, 1970', '50, 1970'])
+make_movie(dir='movie2', imgs = ['compress'], parallel = 8)
 
 # classical example 
 # mixed up friction vs nofriction images with ovito ~ oops!
@@ -63,3 +77,38 @@ make_movie(dir='movie2', imgs = ['compress'])
 # e.g. on using 'startfrom' to specify where image sequence starts from
 # make_movie(dir='movie', imgs = ['beverloo-bottom', 'beverloo-side', 'beverloo-core'], \
 #	imgtxt = ['bottom view', 'side view', 'core view'], imgtxtloc = ['150,990', '150,990', '150,990'], startfrom = 80)
+
+#import ovito
+#import inspect
+
+# The following function is called by OVITO to let the script
+# draw arbitrary graphics content into the viewport.
+# It is passed a QPainter (see http://qt-project.org/doc/qt-5/qpainter.html).
+def render(painter, **args):
+	# This demo code prints the current animation frame
+	# into the upper left corner of the viewport.
+	font = painter.font()
+	font.setPointSize(20.0)
+	painter.setFont(font)
+	
+	xpos = 10
+	ypos = 10 + painter.fontMetrics().ascent()
+	text = "Time: {:.5f} s".format(ovito.dataset.anim.current_frame * 1e-5)
+	painter.drawText(xpos, ypos, text)
+	# The following code prints the current number of particles
+	# into the lower left corner of the viewport.
+	
+	xpos = 10
+	ypos = painter.window().height() - 10
+	if ovito.dataset.selected_node:
+		num_particles = ovito.dataset.selected_node.compute().number_of_particles
+		text = "{} particles".format(num_particles)
+	else:
+		text = "no particles"
+	#painter.drawText(xpos, ypos, text)
+	font.setPointSize(15.0)
+	painter.setFont(font)
+	
+	painter.drawText(xpos + 35, ypos - 110, '0.5 mm')
+
+	# resolution : 800 x 1080
