a06f9791cff552bb7464d9b096c7f646e1ef471b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-02-22 14:48:52 -0500

    added method for XRCT image analysis

--- PyDEM/Analyzer/tools.py
+++ PyDEM/Analyzer/tools.py
@@ -1,15 +1,14 @@
-try:
-	from xlrd import open_workbook
-except:
-	pass
-
 from numbers import Number
+from PIL import Image
+from numpy import random, array, linspace, sqrt, fabs
 
 def readExcel(fname):
 	"""
 	reads an excel sheet(s) and appends each data column to 
 	a dictionary
 	"""
+	from xlrd import open_workbook
+
 	book = open_workbook(fname, on_demand=True)
 	data = {}
 
@@ -31,3 +30,49 @@ def readExcel(fname):
 		data[key] = array(data[key])
 
 	return data
+
+def genImg(Particles, zmin, zmax, scale, output=None):
+	"""
+	Generates a 2D image from a slice (limited by 'zmin/zmax') of a 3D config 
+	in the Particles class. The scale is the number of pixels per meter.
+	"""
+	N = Particles.natoms
+	Particles = Particles[Particles.z > zmin]
+	Particles = Particles[Particles.z < zmax]
+
+	# map particle positions to pixels
+	x = Particles.x
+	x = array(x * scale, 'int')
+
+	y = Particles.y
+	y = array(y * scale, 'int')
+
+	z = Particles.z
+	trans = array(z * 255 / z.max(), 'int')
+
+	zmean = (zmax + zmin) * 0.5
+
+	r = sqrt(Particles.radius**2.0 - (z - zmean)**2.0)
+	r = array(r * scale, 'int')
+
+	length, width = max(y), max(x)
+	img = Image.new('RGBA', (length, width), "black") # create a new black image
+	pixels = img.load() # create the pixel map
+
+	for n in range(N):
+
+		i, j = x[n], y[n]
+		radius = r[n]
+
+		if (i + radius < length and i - radius > 0) and (j + radius < width and j - radius > 0):
+			for ic in range(-radius, radius+1):
+				for jc in range(-radius, radius+1):
+					if (ic)**2 + (jc)**2 <= radius**2:
+			        		pixels[i+ic,j+jc] = (235, 235, 235, trans[n]) # set the colour accordingly
+		else:
+			pass
+
+	img.show()
+
+	if output:
+		img.save(output)
