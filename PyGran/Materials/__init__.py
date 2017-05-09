dfbf72c6eeeb5b01d1aa05b51d1ea2769c5d56d5
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-05-09 12:58:01 -0400

    fixed SA typo

--- PyGran/Materials/__init__.py
+++ PyGran/Materials/__init__.py
@@ -33,20 +33,21 @@ glass = {
 		'coefficientFriction': ('coefficientFriction', 'peratomtypepair', '0.5'),
 		'coefficientRollingFriction': ('coefficientRollingFriction', 'peratomtypepair', '5e-1'),
 		'cohesionEnergyDensity': ('cohesionEnergyDensity', 'peratomtypepair', '0.05'),
-		'coefficientRestitution': (('coefficientRestitution', 'peratomtypepair', '0.9')),
+		'coefficientRestitution': ('coefficientRestitution', 'peratomtypepair', '0.9'),
 		'yieldPress': ('yieldPress', 'peratomtype', '7.0e8'),
 		'characteristicVelocity': ('characteristicVelocity', 'scalar', '0.1'),
 		'density': 2500.0
 		}
 
-stericAcid = {
-		'youngsModulus': ('youngsModulus', 'peratomtype', '41.5e6'),
-		'poissonsRatio': ('poissonsRatio', 'peratomtype', '0.24'),
+stearicAcid = {
+		'youngsModulus': ('youngsModulus', 'peratomtype', '3.84e9'),
+		'poissonsRatio': ('poissonsRatio', 'peratomtype', '0.25'),
 		'coefficientFriction': ('coefficientFriction', 'peratomtypepair', '0.5'),
-		'coefficientRollingFriction': ('coefficientRollingFriction', 'peratomtypepair', '0.5'),
-		'cohesionEnergyDensity': ('cohesionEnergyDensity', 'peratomtypepair', '0.1'),
-		'coefficientRestitution': (('coefficientRestitution', 'peratomtypepair', '0.9')),
-		'yieldPress': ('yieldPress', 'peratomtype', '2.2e6'),
+		'coefficientRollingFriction': ('coefficientRollingFriction', 'peratomtypepair', '0.0'),
+		'cohesionEnergyDensity': ('cohesionEnergyDensity', 'peratomtypepair', '0.03'),
+		'coefficientRestitution': ('coefficientRestitution', 'peratomtypepair', '0.9'),
+		'coefficientRollingViscousDamping': ('coefficientRollingViscousDamping', 'peratomtypepair', '0.1'),
+		'yieldPress': ('yieldPress', 'peratomtype', '5.0e6'),
 		'characteristicVelocity': ('characteristicVelocity', 'scalar', '0.1'),
 		'density': 997.164
 		}
