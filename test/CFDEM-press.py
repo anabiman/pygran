a78193efb610ba30e5984a97fac4f6886ce78a76
Andrew-AbiMansour <andrew.gaam@gmail.com>
2017-06-10 01:02:23 -0400

    defined inlet/outlet and computed inlet velocity

--- test/CFDEM-press.py
+++ test/CFDEM-press.py
@@ -1,15 +1,19 @@
 from PyGran import Analyzer
 from matplotlib.mlab import find
+from scipy.linalg import norm
 
-Fluid = Analyzer.System(mfname='CFD/processor0/VTK/processor0_*.vtk')
+Fluid = Analyzer.System(mfname='/home/levnon/Desktop/VTK/processor0_*.vtk')
 
-# Go to last frame to find inlet / outlet
-Fluid.goto(-1)
-inlet = find(Fluid.Mesh.p == Fluid.Mesh.p.min()).min()
-outlet = find(Fluid.Mesh.p == Fluid.Mesh.p.max()).max()
+# Compute center of each mesh cell
+cellCenter = Fluid.Mesh.CellsPos.mean(axis=1)
+
+inlet = find(cellCenter[:,2] == cellCenter[:,2].min())
+outlet = find(cellCenter[:,2] == cellCenter[:,2].max())
 
 # Loop over mesh trajectory and compute the pressure drop
-pressure = []
+pressDrop = []
+velInlet = []
 
 for ts in Fluid:
-	pressure.append(Fluid.p[outlet] - Fluid.p[inlet])
+	pressDrop.append(Fluid.Mesh.p[inlet].mean() - Fluid.Mesh.p[outlet].mean())
+	velInlet.append(norm(Fluid.Mesh.U[inlet,:].mean(axis=0)))
