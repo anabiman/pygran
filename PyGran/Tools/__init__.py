1c430d6061c09e41b8869b6a0da284313f0b85a2
Andrew-AbiMansour <anabiman@indiana.edu>
2017-07-15 02:28:37 -0400

    added si-micro conversion factors

--- PyGran/Tools/__init__.py
+++ PyGran/Tools/__init__.py
@@ -1,5 +1,17 @@
 import os
 
+# Conversion systems
+# S.I. to microns
+siToMicro = {'distance': 1e6, \
+			'time': 1e6, \
+			'mass': 1e6, 
+}
+
+microToSi = {'distance': 1e-6, \
+			'time': 1e-6, \
+			'mass': 1e-6, 
+}
+
 def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
