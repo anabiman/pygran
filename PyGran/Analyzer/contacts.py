2c1d78ec0bc79b0efc38bb49f19a9399b3f00265
Andrew-AbiMansour <anabiman@indiana.edu>
2017-07-23 17:31:17 -0400

    force chain bugs fixed; 2D plots added

--- PyGran/Analyzer/contacts.py
+++ PyGran/Analyzer/contacts.py
@@ -31,6 +31,7 @@ from scipy import spatial
 from scipy.linalg import norm
 from PyGran.Simulator.models import SpringDashpot
 import matplotlib.pylab as plt
+import networkx as nx
 
 class Neighbors(object):
 	""" A dynamic class that contains all the particle-particle (and optionally particle-wall)
@@ -82,12 +83,12 @@ class Neighbors(object):
 	def overlaps(self):
 	    return self._overlaps
 
-	def forceChain(self, axis = (0,2), alpha = 45):
+	def forceChain(self, axis = (0,2), alpha = numpy.pi/4, plot_stress=True, plot_parts=False, peters=True, threshold=1):
 		""" Computes the force chain based on an algorithm published in Phys. Rev. E. 72, 041307 (2005):
 		'Characterization of force chains in granular material'.
 
 		@ axis: a tuple of size 2 that specifies the two axis to use for computing the force chain, e.g. axis=(0,1) -> (x,y)
-		@ alpha: the angle (in degrees) that controls the deviation of the force chain. A value of 0 means a perfectly linear chain.
+		@ alpha: the angle (in radians) that controls the deviation of the force chain. A value of 0 means a perfectly linear chain.
 		Thus, alpha is a measure of the 'curvature' of the force chain. See page 5 of the paper cited above.
 		"""
 
@@ -106,8 +107,8 @@ class Neighbors(object):
 			vi = 4.0/3.0 * numpy.pi * self._Particles.radius[i]**3.0
 			vj = 4.0/3.0 * numpy.pi * self._Particles.radius[j]**3.0
 
-			stress_i = numpy.outer(self._model.normalForce(overlap, self._Particles.radius[i]), overlap)
-			stress_j = numpy.outer(self._model.normalForce(overlap, self._Particles.radius[j]), overlap)
+			stress_i = numpy.outer(self._model.normalForce(overlap, self._Particles.radius[i]), overlap) / vi
+			stress_j = numpy.outer(self._model.normalForce(overlap, self._Particles.radius[j]), overlap) / vj
 
 			stress[i] += stress_i
 			stress[j] += stress_j
@@ -118,109 +119,160 @@ class Neighbors(object):
 			# Compute principal stress
 			stress_i = stress[i]
 
-			stress_p = 0.5 * (stress_i[0, 0] + stress_i[1, 1]) - numpy.sqrt( (0.5 * (stress_i[0, 0] - \
+			stress_p = 0.5 * (stress_i[0, 0] + stress_i[1, 1]) + numpy.sqrt( (0.5 * (stress_i[0, 0] - \
 				stress_i[1, 1]))**2.0 + stress_i[0, 1] )
 
 			if stress_i[0,0] - stress_i[1,1]: # otherwise this is prolly a boundary particle
 
 				# Compute angle
 				stress_prin[i,2] = 0.5 * numpy.arctan( 2.0 * stress_i[0,1] / (stress_i[0,0] \
-								  - stress_i[1,1]) ) * 360.0
+								  - stress_i[1,1]) )
+
 				# compute principal stress
 				stress_prin[i,0] = stress_p * numpy.cos(stress_prin[i,2])
 				stress_prin[i,1] = stress_p * numpy.sin(stress_prin[i,2])
 
 		stress_norm = norm(stress_prin[:,:-1], axis=1)
 
-		# Section V. THE ALGORITHM (page 4)
-		# Step 1: filter out particles with less than the mean principal stress
-		indices  = numpy.where(stress_norm >= stress_norm.mean(axis=0))[0]
-		#stress_prin = stress_prin[indices,:]
+		if not peters: # plot all particle stresses
+			
+			indices  = numpy.where(stress_norm > 0)[0]
+			Particles = self._Particles[indices]
 
-		# Step 2: filter out particles which are in contact with 1 or less 'highly stressed' particles
-		# Construct an nns list
-		coords = self._coords[indices,:][:,axis] 
-		tree = spatial.cKDTree(coords)
+			coords = numpy.array([Particles.x, Particles.y, Particles.z])
+			x,y = coords[axis[0]], coords[axis[1]]
 
-		nns = tree.query_ball_point(coords, self._Particles[indices].radius.max() * 2.0)
-		self._indices = indices
-		self._nns = [[] for i in range(len(nns))]
+			stress_prin = stress_prin[indices,:]
+			stress_norm = stress_norm[indices]
 
-		# Update the nns indices so that only overlapping neighbors remain
-		count = 0
-		saxis = numpy.array(axis)
+			stress_norm /= stress_norm.max() * 0.3
 
-		for ns in nns:
-			pind = indices[count]
-			for index in ns:
-				if index != pind:
+			plt.axes()
 
-					dist = numpy.sqrt(((self._coords[index,saxis] - self._coords[pind,saxis])**2.0).sum())
+			for i in range(coords.shape[1]):
+				theta = stress_prin[i,-1]
+				xi = [x[i] - Particles.radius[i] * numpy.cos(theta), x[i] + Particles.radius[i] * numpy.cos(theta)]
+				yi = [y[i] - Particles.radius[i] * numpy.sin(theta), y[i] + Particles.radius[i] * numpy.sin(theta)]
 
-					if dist <= (self._Particles.radius[index] + self._Particles.radius[pind]):
-						self._nns[count].append(index)
+				if plot_stress:
+					plt.plot(xi, yi, linewidth=stress_norm[i], color='black')
 
-			count += 1
+				if plot_parts:
+					circle = plt.Circle((x[i], y[i]), radius=Particles.radius[i], fill=False, color='blue', linewidth=0.5, linestyle ='dotted')
+					plt.gca().add_patch(circle)
 
-		count = 0
-		cosAlpha = numpy.cos(alpha)
-		chain = [[] for i in range(len(self._nns))]
+					circle = plt.Circle((x[i], y[i]), radius=Particles.radius[i], fill=False, color='blue', linewidth=0.5, linestyle ='dotted')
+					plt.gca().add_patch(circle)
 
-		# Step 3: compute the force chain per particle
-		for nns in self._nns:
-			if len(nns) > 1: # select only particles surrounded by 2 or more highly stressed particles
+			plt.axis('scaled')
+			plt.show()
+
+			return stress_prin
+
+
+
+		else: # us Peters' algorithm for computing force chain propagation
+			# Section V. THE ALGORITHM (page 4)
+			# Step 1: filter out particles with less than the mean principal stress
+			indices  = numpy.where(stress_norm >= threshold * stress_norm.mean(axis=0))[0]
+
+			stress_norm = stress_norm[indices]
+
+			# Step 2: filter out particles which are in contact with 1 or less 'highly stressed' particles
+			# Construct an nns list
+			coords = self._coords[indices,:][:,axis] 
+			stress_prin = stress_prin[indices,:]
+			radii = self._Particles.radius[indices]
+
+			tree = spatial.cKDTree(coords)
+
+			nns = tree.query_ball_point(coords, self._Particles[indices].radius.max() * 2.0)
+			self._nns = [[] for i in range(len(nns))]
+
+			# Update the nns indices so that only overlapping neighbors remain
+			count = 0
+
+			for ns in nns:
+				for index in ns:
+					if index != count:
+
+						dist = norm(coords[index] - coords[count])
+
+						if dist <= (radii[index] + radii[count]):
+							self._nns[count].append(index)
+
+				self._nns[count].insert(0, count)
+				count += 1
+
+			cosAlpha = numpy.cos(alpha)
+			chain = []
+
+			# Step 3: Remove overlapping particles with 2 or less neighbors 
+			remInd = []
+			for nns in self._nns:
+				if len(nns) <= 2: # select only particles surrounded by 2 or more highly stressed particles
+					remInd.append(nns)
+
+			for ind in remInd:
+				self._nns.remove(ind)
+
+			# Step 4: compute the force chain per particle
+			for nns in self._nns:
+				
+				index = nns[0]
 				# Begin loop over each neighboring particle
-				for ni in nns:
+				for ni in nns[1:]:
+
 					# check for angle compliance
-					angle_i = stress_prin[indices[count],-1]
+					angle_i = stress_prin[index,-1]
 					angle_j =  stress_prin[ni,-1]
 
-					dist = self._coords[indices[count], saxis] - self._coords[ni,saxis]
+					dist = coords[ni] - coords[index]
 
-					term1 = norm(dist * stress_prin[indices[count],:-1])
-					term2 = norm(dist) * norm(stress_prin[indices[count],:-1])
+					term1 = numpy.fabs(numpy.dot(dist, stress_prin[index,:-1]))
+					term2 = norm(dist) * stress_norm[index]
 
-					term3 = norm(- dist * stress_prin[ni,:-1])
-					term4 = norm(dist) * norm(stress_prin[ni,:-1])
+					term3 = numpy.fabs(numpy.dot(-dist, stress_prin[ni,:-1]))
+					term4 = norm(-dist) * stress_norm[ni]
 
 					# Search in the forward direction
 					if (term1 <= term2) and (cosAlpha * term2) < term1:
 						if (term3 <= term4) and (cosAlpha * term4) < term3:
-							chain[count].append(ni)
-
+							chain.append((index,ni))
+					
 					# Search in the reverse direction
-					if (term1 <= term2) and (-cosAlpha * term2) > term1: # TODO: double check the math on this one
-						if (term3 <= term4) and (-cosAlpha * term4) > term3: # TODO: double check the math on this one
-							chain[count].append(ni)
+					# I dont understand what Im doing here
+					#if (term1 <= term2) and (-cosAlpha * term2) > term1: # TODO: double check the math on this one
+						#if (term3 <= term4) and (-cosAlpha * term4) > term3: # TODO: double check the math on this one
+							#chain.append((index,ni))
+							#print (index,ni)
+							
+			stress_norm /= stress_norm.max() * 0.8
 
-			count += 1
+			plt.axes()
 
-		parts = self._Particles[indices]
-		plt.scatter(parts.x *  1e6, parts.z * 1e6, s=parts.radius * 1e6, facecolors='none')
+			chain = numpy.unique(chain)
+			print 'Natoms highly stressed = {} out of {} particles'.format(len(chain), self._Particles.natoms)
 
-		count = 0
-		for ind in chain:
-			for i in ind:
-				plt.plot([stress_prin[count,0], stress_prin[count,1]] ,'r')
+			for i in chain:
+				theta = stress_prin[i,-1]
+				x = [coords[i,0] - radii[i] * numpy.cos(theta), coords[i,0] + radii[i] * numpy.cos(theta)]
+				y = [coords[i,1] - radii[i] * numpy.sin(theta), coords[i,1] + radii[i] * numpy.sin(theta)]
 
-		plt.show()
+				if plot_stress:
+					plt.plot(x, y, linewidth=stress_norm[i], color='black')
 
-		#import networkx as nx
-		#import numpy as np
-		#import matplotlib.pyplot as plt
+				if plot_parts:
+					circle = plt.Circle((coords[i,0], coords[i,1]), radius=radii[i], fill=False, color='blue', linewidth=0.5, linestyle ='dotted')
+					plt.gca().add_patch(circle)
 
-		#G = nx.Graph()
-		#G.add_edges_from([(1,2), (1,3)])
+					circle = plt.Circle((coords[i,0], coords[i,1]), radius=radii[i], fill=False, color='blue', linewidth=0.5, linestyle ='dotted')
+					plt.gca().add_patch(circle)
 
-		#pos = {1: (40, 20), 2: (20, 30), 3: (40, 30)}
+			plt.axis('scaled')
+			plt.show()
 
-		#nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'))
-		#nx.draw_networkx_edges(G, pos, edgelist=((1,2),(1,3)), width=(1.0,5.0), edge_color='black', arrows=True)
-
-		#plt.show()
-
-
-		return chain
+			return chain, stress_prin
 
 	def findWithin(self, coords , r):
 		""" Find all points within distance r of point(s) coords.
