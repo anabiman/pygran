97d4871b6bc9fba57c57e214aabcaccf161bcee2
Andrew-AbiMansour <anabiman@indiana.edu>
2017-07-25 01:39:17 -0400

    added yield overlap calculation script

--- test/contacts.py
+++ test/contacts.py
@@ -2,9 +2,9 @@ from PyGran import Analyzer
 from PyGran.Materials import glass
 
 System = Analyzer.System('traj.dump')
-
-# Go to last frame
 System.goto(-1)
 
-Neigh = Analyzer.Neighbors(System.Particles, material=glass)
+Neigh = Analyzer.Neighbors(System.Particles)
+overlaps, i, j = Neigh.overlaps
+
 
---
+++ test/yielding.py
@@ -0,0 +1,60 @@
+from PyGran import Analyzer
+from PyGran.Materials import stearicAcid
+from numpy import pi, sqrt
+
+def checkYield(reff, **material):
+	""" Solves the cubic equation x^3 - b*x - c = 0 for yielding contact_radius = sqrt(x)
+	based on Thornton's elasto-plastic cohesive model:
+
+	b = py * pi * reff / (2 * YoungEff)
+	c = reff * sqrt(gamma * pi / (2 * YoungEff))
+
+	@reff: effective radius
+	@py: yielding pressure
+	@YoungEff: Young's effective modulus
+	@gamma: cohesion energy density
+	"""
+	# Extract material params from supplied database
+	py = material['yieldPress']
+	poiss = material['poissonsRatio']
+	gamma = material['cohesionEnergyDensity']
+	Young = material['youngsModulus']
+	YoungEff = Young * 0.5 / (1.0  - poiss)
+
+	# Compute the 'b' and 'c' coefficients
+	b = py * pi * reff / (2 * YoungEff)
+	c = reff * sqrt(gamma * pi / (2 * YoungEff))
+
+	# Solve the algebraic equation symbolically
+	frac = 0.333333333333333
+	common = (9*c + sqrt(3*(27*c**3 - 4*b**2)))**frac
+	x = b * (2.0/3.0)**frac /  common + common / (18**frac)
+
+	# Compute contact yield radius
+	ay = sqrt(x)
+
+	# Return yield overlap
+	return x / reff - sqrt(2 * pi * gamma * ay / YoungEff)
+
+
+System = Analyzer.System('../../compaction/out-SpringDashpot/traj/traj.dump')
+System.goto(-1)
+
+Neigh = Analyzer.Neighbors(System.Particles)
+overlaps, indices = Neigh.overlaps[:,0], Neigh.overlaps[:,1:]
+
+# Extract radii of all particles
+radii = System.Particles.radius
+
+for contact, index in enumerate(indices):
+
+	# Get the two particle (in contact) indices
+	i,j = index
+
+	# Compute reff
+	reff = 1.0 / radii[i] + 1.0 / radii[j]
+
+	# Compute yielding overlap
+	delta_y = checkYield(reff, **stearicAcid)
+
+	print overlaps[contact], delta_y
