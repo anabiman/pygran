499b1805adfdb54f26ee09d5d2f815023075c00d
Andrew-AbiMansour <anabiman@indiana.edu>
2017-07-31 00:01:23 -0400

    rand() now generates non-overlapping configs

--- PyGran/Simulator/generator.py
+++ PyGran/Simulator/generator.py
@@ -1,15 +1,15 @@
 from PyGran import Analyzer
-from numpy import random, ones, zeros, sqrt
-from sys import argv
+import numpy
+from numpy import random
 import collections
 
-
-def random(natoms, radius, overlapping=True):
+def rand(natoms, radius, overlapping=True, factor=1.0):
 	""" Creates a set of randomly placed non-overlapping particles 
 
 	@natoms: number of particles to create
 	@radius: a float or array (of size Natoms)
 	@[overlapping]: boolean variable which ensures all particles are allowed to overlap or natoms
+	@[factor]: float to scale noise (w.r.t particle radius) by when correcting for overlapping particles
 
 	Returns a Particles object.
 	"""
@@ -19,7 +19,7 @@ def random(natoms, radius, overlapping=True):
 	x,y,z = random.rand(natoms) * radius * scale, random.rand(natoms) * radius * scale, random.rand(natoms) * radius * scale
 
 	if type(radius) is float or type(radius) is int:
-		r = ones(natoms) * radius
+		r = numpy.ones(natoms) * radius
 	elif len(r) == natoms:
 		r = radius
 	else:
@@ -31,7 +31,42 @@ def random(natoms, radius, overlapping=True):
 	data['z'] = z
 	data['radius'] = r
 	
-	return Analyzer.Particles(**data)
+	if not overlapping:
+
+		Particles = Analyzer.Particles(**data)
+		indices_tot = numpy.arange(len(Particles))
+
+		while(True):
+			
+			Neigh = Analyzer.Neighbors(Particles)
+		
+			if len(Neigh.overlaps):
+				indices = numpy.array(Neigh.overlaps[:,1:], 'int')
+				indices.reshape(len(indices)*2)
+				indices = numpy.unique(indices)
+ 
+ 				Particles_overlap = Particles[indices]
+
+ 				# Find all particle indices not overlapping
+ 				# Is there a faster way to do this?
+ 				indices_rem = []
+ 				for i in indices_tot:
+ 					if i not in indices:
+ 						indices_rem.append(i)
+
+ 				Particles = Particles[indices_rem]
+
+				Particles_overlap.perturb(factor * Particles_overlap.radius)
+
+				Particles += Particles_overlap
+				
+				print len(Neigh.overlaps)
+
+			else:
+				return Particles
+
+	else:
+		return Analyzer.Particles(**data)
 
 def hcp(natoms, radius):
 	""" Generates a Hexagonal Close Packed structure
@@ -45,16 +80,16 @@ def hcp(natoms, radius):
 	N = int(natoms**(1/3.0))
 	count = 0
 
-	data['x'], data['y'], data['z'] = zeros(natoms), zeros(natoms), zeros(natoms)
+	data['x'], data['y'], data['z'] = numpy.zeros(natoms), numpy.zeros(natoms), numpy.zeros(natoms)
 
 	for i in range(N):
 		for j in range(N):
 			for k in range(N):
 				data['x'][count] = (2.0 * i + ((j + k) % 2)) * radius
-				data['y'][count] = (sqrt(3.) * (j + 1.0/3.0 * (k % 2))) * radius
-				data['z'][count] = (2.0 * sqrt(6.0) / 3.0 * k) * radius
+				data['y'][count] = (numpy.sqrt(3.) * (j + 1.0/3.0 * (k % 2))) * radius
+				data['z'][count] = (2.0 * numpy.sqrt(6.0) / 3.0 * k) * radius
 				count += 1
 
-	data['radius'] = ones(natoms) * radius
+	data['radius'] = numpy.ones(natoms) * radius
 
 	return Analyzer.Particles(**data)
