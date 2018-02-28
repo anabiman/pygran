d8e65b7706cb0077d44815cfdd61e22a3d735215
Andrew-AbiMansour <andrew.gaam@gmail.com>
2018-02-28 11:14:10 -0500

    added ovito module

--- PyGran/Visualizer/__init__.py
+++ PyGran/Visualizer/__init__.py
@@ -21,4 +21,4 @@ Created on July 11, 2016
 
 # -------------------------------------------------------------------------
 
-from PyGran.Visualizer.visualize import *
+from PyGran.Visualizer.ovito import *
