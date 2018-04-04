586aec900ef57f99d5193f563fab9f767579c8d5
Andrew-AbiMansour <andrew.gaam@gmail.com>
2018-04-04 00:46:26 -0400

    added rotating/blending tutorial

--- examples/scripts/flow/rot-silo.py
+++ examples/scripts/flow/rot-silo.py
@@ -7,55 +7,54 @@ Created on April 22, 2017
 # -*- coding: utf8 -*-
 
 from PyGran import Simulator, Visualizer
-from PyGran.Materials import glass, stearicAcid
-import os
+from PyGran.Materials import organic, glass
 
-glass['youngsModulus'] = 1e9
+params = {
+	# Define the system
+	'boundary': ('f','f','f'),
+	'box':  (-1e-3, 1e-3, -1e-3, 1e-3, 0, 4e-3),
 
-pDict = {
-		# Define the system
-		'boundary': ('f','f','f'),
-		'box':  (-1e-3, 1e-3, -1e-3, 1e-3, 0, 4e-3),
+	# Define component(s)
+	'SS': ({'material': organic, 'radius': ('constant', 5e-5)},),
 
-		# Define component(s)
-		'SS': ({'material': stearicAcid, 'radius': ('constant', 5e-5)},),
+	# Setup I/O params
+	#'traj': {'freq':1000, 'style': 'custom/vtk', 'pfile': 'traj*.vtk', 'mfile': 'mesh*.vtk'},
+	'traj': {'freq':1000, 'pfile': 'traj.dump', 'mfile': 'mesh*.vtk'},
 
-		# Setup I/O params
-		'traj': {'freq':1000, 'style': 'custom/vtk', 'pfile': 'traj*.vtk', 'mfile': 'mesh*.vtk'},
-		'output': 'test',
+	# Define computational parameters
+	'dt': 1e-6,
 
-		# Define computational parameters
-		'dt': 1e-6,
+	# Apply a gravitional force in the negative direction along the z-axis
+	'gravity': (9.81, 0, 0, -1),
 
-		# Apply a gravitional force in the negative direction along the z-axis
-		'gravity': (9.81, 0, 0, -1),
+	# Import hopper mesh
+	 'mesh': {
+		'hopper': {'file': 'mesh/silo.stl', 'mtype': 'mesh/surface', 'material': glass, 'args': ('scale 1e-3',)},
+		'impeller': {'file': 'mesh/valve.stl', 'mtype': 'mesh/surface', 'material': glass, 'args': ('move 0 0 1.0', 'scale 1e-3',)},
+	},
 
-		# Import hopper mesh
-		 'mesh': {
-			'hopper': {'file': 'silo.stl', 'mtype': 'mesh/surface', 'import': True, 'material': glass, 'args': ('scale 1e-3',)},
-			'valve': {'file': 'valve.stl', 'mtype': 'mesh/surface', 'import': True, 'material': glass, 'args': ('move 0 0 1.0', 'scale 1e-3',)},
-		      },
-
-		# Stage runs
-		'stages': {'insertion': 1e4, 'run': 1e4},
-	  }
+	# Stage runs
+	'stages': {'insertion': 1e5, 'run': 1e5},
+}
 
 # Create an instance of the DEM class
-sim = Simulator.DEM(**pDict)
+sim = Simulator.DEM(**params)
 
 # Setup a stopper wall along the xoy plane
 stopper = sim.setupWall(species=1, wtype='primitive', plane = 'zplane', peq = 0.0)
 
-sim.moveMesh('valve', 'rotate origin 0. 0. 0.', 'axis  0. 0. 1.', 'period 5e-2')
-
 # Insert particles in a cubic region
-insert = sim.insert(species=1, region=('block', -5e-4, 5e-4, -5e-4, 5e-4, 2e-3, 3e-3), value=100)
-sim.run(pDict['stages']['insertion'], pDict['dt'])
+insert = sim.insert(species=1, region=('block', -5e-4, 5e-4, -5e-4, 5e-4, 2e-3, 3e-3), mech='volumefraction_region', value=1, freq=1e4)
+sim.run(params['stages']['insertion'], params['dt'])
 sim.remove(insert)
 
-# Run equilibration run
-sim.run(pDict['stages']['run'], pDict['dt'])
+# Rotate the impeller
+rotImp = sim.moveMesh('impeller', 'rotate origin 0. 0. 0.', 'axis  0. 0. 1.', 'period 5e-2')
+
+# Blend the system
+sim.run(params['stages']['run'], params['dt'])
+sim.remove(rotImp)
 
 # Remove stopper and monitor flow
 sim.remove(stopper)
-sim.run(pDict['stages']['run'], pDict['dt'])
+sim.run(params['stages']['run'], params['dt'])
