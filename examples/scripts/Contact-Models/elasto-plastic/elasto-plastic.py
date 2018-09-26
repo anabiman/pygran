7fb4c1d05946460d98b9fe50313fd22f25360a43
Andrew-AbiMansour <andrew.gaam@gmail.com>
2018-09-26 00:28:58 -0400

    updated elasto-plastic model

--- examples/scripts/Contact-Models/elasto-plastic/elasto-plastic.py
+++ examples/scripts/Contact-Models/elasto-plastic/elasto-plastic.py
@@ -1,6 +1,6 @@
 import PyGran.Simulator as Sim
-from PyGran.Materials import organic
-from numpy import arange, fabs
+from PyGran.Params import cohesionless
+from numpy import arange, fabs, array, sqrt
 import matplotlib.pylab as plt
 import matplotlib as mpl
 
@@ -10,30 +10,49 @@ plt.rcParams.update({'font.size':18})
 
 cModel = Sim.models.ThorntonNing
 
-COR = []
+COR, yieldVel = [], []
 
 # Create a yield pressure array to study
-pressure = arange(1, 6, 0.1) * organic['youngsModulus'] * 0.01
+pressure = arange(1, 6, 0.1) * cohesionless['youngsModulus'] * 0.01
 
 # Set particle radius to 100 microns
-organic['radius'] = 1e-4
+cohesionless['radius'] = 1e-4
+cohesionless['coefficientRestitution'] = 0.5
 
 for yieldPress in pressure:
 
-	organic['yieldPress'] = yieldPress
-	model = cModel(material=organic)
+	yeff = cohesionless['youngsModulus'] * 0.5 / (1. - cohesionless['poissonsRatio']**2)
 
-	time, disp, force = model.displacement()
+	cohesionless['yieldPress'] = yieldPress
+	model = cModel(material=cohesionless)
+
+	time, disp, force = model.displacement(dt=2.33322158839e-07)
+
 	deltav = disp[:,1]
 
-	COR.append(fabs(deltav[-1] / deltav[0]))
+	COR.append(fabs(deltav[-1] / cohesionless['characteristicVelocity']))
+	yieldVel.append( model.yieldVel / cohesionless['characteristicVelocity'] )
+
+ratio = array(yieldVel)
+ratio[ratio > 1] = 1
+
+COR_anal = sqrt(6. * sqrt(3.0) / 5.0) * sqrt(1.0 - (1.0/6.0) * (ratio)**2) * \
+    (ratio / (ratio + 2 * sqrt( 6.0/5.0 - (1.0/5.0) * ratio**2 ) ) )**(0.25)
+
 
 fig = plt.figure(figsize=(16,18), dpi=80)
-plt.plot(pressure / 1e6, COR, 'o-')
+
+#print(COR_anal * cohesionless['characteristicVelocity'], fabs(deltav[-4::]), deltav[0])
+
+#plt.plot( deltav / cohesionless['characteristicVelocity']  )
+plt.plot(pressure, COR, '-o')
+plt.plot(pressure, COR_anal)
+
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.xlabel('Yield Pressure (MPa)')
 plt.ylabel('Coefficient of Restitution')
 plt.grid(linestyle=':')
+plt.legend(['Numerical', 'Analytical'])
 plt.show()
 
