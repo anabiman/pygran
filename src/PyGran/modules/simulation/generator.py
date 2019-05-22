276ca4278a0b43a568fea6c1adc14539d6ddb2a5
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-05-22 10:17:40 -0400

    fixed tabbing

--- src/PyGran/modules/simulation/generator.py
+++ src/PyGran/modules/simulation/generator.py
@@ -1,25 +1,32 @@
 # !/usr/bin/python
 # -*- coding: utf8 -*- 
-# -----------------------------------------------------------------------
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 2 of the License, or
-#   (at your option) any later version.
-
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# -------------------------------------------------------------------------
 
 '''
 Created on March 30, 2016
 @author: Andrew Abi-Mansour
+
+  This is the 
+   __________         ________                     
+  ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+  ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+  ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+  ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+  ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+  ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                      
+  DEM simulation and analysis toolkit
+  http://www.pygran.org, support@pygran.org
+
+  Core developer and main author:
+  Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+  PyGran is open-source, distributed under the terms of the GNU Public
+  License, version 2 or later. It is distributed in the hope that it will
+  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+  received a copy of the GNU General Public License along with PyGran.
+  If not, see http://www.gnu.org/licenses . See also top-level README
+  and LICENSE files.
 '''
 
 from PyGran import analysis
@@ -69,23 +76,21 @@ def rand(natoms, radius, overlapping=True, factor=1.0):
 				indices.reshape(len(indices)*2)
 				indices = numpy.unique(indices)
  
- 				Particles_overlap = Particles[indices]
+				Particles_overlap = Particles[indices]
 
- 				# Find all particle indices not overlapping
- 				# Is there a faster way to do this?
- 				indices_rem = []
- 				for i in indices_tot:
- 					if i not in indices:
- 						indices_rem.append(i)
+				# Find all particle indices not overlapping
+				# Is there a faster way to do this?
+				indices_rem = []
+				for i in indices_tot:
+					if i not in indices:
+						indices_rem.append(i)
 
- 				Particles = Particles[indices_rem]
+				Particles = Particles[indices_rem]
 
 				Particles_overlap.perturb(factor * Particles_overlap.radius)
 
 				Particles += Particles_overlap
 				
-				print len(Neigh.overlaps)
-
 			else:
 				return Particles
 
