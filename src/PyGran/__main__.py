cfa2c7f3ec6647b4b1f7d84a6f9211fce61ff230
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-09-27 19:05:58 -0400

    removed junk from __main__ and added try/except to imports in __init__

--- src/PyGran/__main__.py
+++ src/PyGran/__main__.py
@@ -26,17 +26,4 @@
   and LICENSE files.
 '''
 
-
-import GUI.windows as windows
-import wx
-import GUI.Settings.Language as Lang
-
-if __name__ == '__main__':
-
-        app = wx.App(redirect=False)
-
-        win = windows.DerivedWindow(parent=None,name=Lang.__SofName__, title=Lang.__SofName__)
-        win.Show()
-
-        app.MainLoop()
-
+from .__meta__ import *
