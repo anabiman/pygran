4f424d496556df5d1ce50434318953d1fccd6215
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-08 16:44:56 -0400

    added _version.py

---
+++ src/PyGran/_version.py
@@ -0,0 +1,31 @@
+'''
+  Created on Oct 5, 2019
+  Author: Andrew Abi-Mansour
+
+  This is the::
+
+  ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+  ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+  ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+  ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+  ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+  ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+  DEM simulation and analysis toolkit
+  http://www.pygran.org, support@pygran.org
+
+  Core developer and main author:
+  Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+  PyGran is open-source, distributed under the terms of the GNU Public
+  License, version 2 or later. It is distributed in the hope that it will
+  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+  received a copy of the GNU General Public License along with PyGran.
+  If not, see http://www.gnu.org/licenses . See also top-level README
+  and LICENSE files.
+  '''
+
+__author__ = 'Andrew Abi-Mansour'
+__version__ = '1.2.0'
+__email__ = 'support@pygran.org'
