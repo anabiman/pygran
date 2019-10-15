b298a6458037c3799afb93d608e8e336d8d4132b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-15 16:13:06 -0400

    renamed _autosummary to autosummary

--- docs/sphinx/fix.sh
+++ docs/sphinx/fix.sh
@@ -1,7 +1,8 @@
 grep -rl '_static' build/html | xargs sed -i 's/_static/static/g'
 grep -rl '_images' build/html | xargs sed -i 's/_images/images/g'
+grep -rl '_autosummary' build/html | xargs sed -i 's/_autosummary/autosummary/g'
 grep -rl '2014 drove.io' build/html | xargs sed -i 's/2014 drove.io/2019 PyGran/g'
 
 mv build/html/_static build/html/static
 mv build/html/_images build/html/images
-
+mv build/html/_autosummary build/html/autosummary
