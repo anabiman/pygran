904e2f2bac70112a6cd7b551e82e90bf7b8cf222
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-25 18:33:54 -0400

    improve code

--- pygran/tests/test_analysis/test_particles_struct.py
+++ pygran/tests/test_analysis/test_particles_struct.py
@@ -1,24 +1,23 @@
-from PyGran import analysis
-import sys
+from pygran import analysis
 
 
 def test_foo(trajf):
     # Create a granular object from a LIGGGHTS dump file
-    Sys = analysis.System(Particles=trajf, units="si")
+    sys = analysis.System(Particles=trajf, units="si")
 
     # Go to last frame
-    Sys.goto(-1)
+    sys.goto(-1)
 
     # Switch to micro unit system
-    Sys.units("micro")
+    sys.units("micro")
 
     # Compute the radial distribution function
-    g, r, _ = Sys.Particles.computeRDF()
+    g, r, _ = sys.Particles.computeRDF(optimize=False)
 
     assert r.max() > 4.0
 
     # Construct a class for nearest neighbor searching
-    Neigh = analysis.equilibrium.Neighbors(Sys.Particles)
+    neigh = analysis.equilibrium.Neighbors(sys.Particles)
 
     # Extract coordination number per particle
-    coon = Neigh.coon
+    coon = neigh.coon
