88c26288c773088c480010b08251e35aa7ec0097
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-25 18:26:52 -0400

    Create simulation module

---
+++ pygran/simulation.py
@@ -0,0 +1,37 @@
+"""
+  Created on Sep 25, 2021
+  Author: Andrew Abi-Mansour
+
+  This is the::
+
+  ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+  ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+  ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+  ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+  ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+  ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+  DEM simulation and analysis toolkit
+  http://www.pygran.org, support@pygran.org
+
+  Core developer and main author:
+  Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+  PyGran is open-source, distributed under the terms of the GNU Public
+  License, version 2 or later. It is distributed in the hope that it will
+  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+  received a copy of the GNU General Public License along with PyGran.
+  If not, see http://www.gnu.org/licenses . See also top-level README
+  and LICENSE files.
+"""
+import warnings
+
+try:
+    import pygran_sim as simulation
+    from pygran_sim import *
+except Exception:
+    simulation = None
+    warnings.warn(
+        "pygran_simulation uninstalled. Solve by: pip install pygran_simulation."
+    )
